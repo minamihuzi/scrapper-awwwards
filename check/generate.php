37c3258509a5022b09e42f18b31b35fdf773a8ae
minamihuzi <59735572+minamihuzi@users.noreply.github.com>
2020-02-17 11:37:35 +0300

    Revert "first step"
    This reverts commit 4fd512e2005990ba0e5066b141eb190d6a87fca4.

--- check/generate.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-    header("Content-Type: application/octet-stream");
-    header("Content-disposition: attachment; filename=\"domains-output.txt\"");
-    header("Content-Transfer-Encoding: Binary"); 
-    readfile('domains-output.txt');
-
-    EXIT();
- ?>
